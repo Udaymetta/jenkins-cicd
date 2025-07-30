7f314866ce513d5ceeebf4f46fe87e5588e9c554
Udaymetta <mudayk99@gmail.com>
2025-07-30 17:58:17 +0530

    Initial commit

---
+++ src/main/java/com/jenkins/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class Controller {
+
+    @GetMapping
+    public String health(){
+        return "Application is Running!!!";
+    }
+}
